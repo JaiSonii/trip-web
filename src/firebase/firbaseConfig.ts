6754dfa9e3ab990d6830f6bd52d012ea8c18adc6
Jai Soni <138607981+JaiSonii@users.noreply.github.com>
2024-07-06 15:11:35 +0530

    Update firbaseConfig.ts

--- src/firebase/firbaseConfig.ts
+++ src/firebase/firbaseConfig.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMKDWAsAbYjojj4qQ3d5xIu4sL1L1GZnA",
-  authDomain: "transportbook-cb28e.firebaseapp.com",
-  projectId: "transportbook-cb28e",
-  storageBucket: "transportbook-cb28e.appspot.com",
-  messagingSenderId: "140819034711",
-  appId: "1:140819034711:web:4584d5c97e900cd5a4a912",
-  measurementId: "G-7WLLHVJY7V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
