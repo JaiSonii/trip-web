e82c5d22af6b386f302ee7cdc956a0b772718fd3
Jai Soni <138607981+JaiSonii@users.noreply.github.com>
2024-07-09 19:18:12 +0530

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
 import Image from 'next/image';
 import notfound from '../assets/not-found.webp';
 
@@ -10,17 +9,14 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
+  
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-100 to-gray-100 p-4">
       <div className="max-w-md text-center p-8 bg-white shadow-2xl rounded-lg">
         <Image src={notfound} alt="Not Found" width={480} height={360} className="mx-auto rounded" />
         <h1 className="text-5xl font-bold text-gray-800 mt-6">404</h1>
-        <p className="text-lg text-gray-600 mt-2">Oops! The page you&apos;re looking for doesn&apos;t exist.</p>
+        <p className="text-lg text-gray-600 mt-2">Oops! The page you are looking for doe not exist.</p>
         <Link href="/" passHref>
           <a className="mt-4 inline-block px-4 py-2 bg-gray-700 text-gray-200 rounded-md hover:bg-gray-600 hover:text-white transition duration-200 shadow-lg">
             Go Home
