72e20ed91cd0538ac84d39417f187f112b621ae9
Jai Soni <138607981+JaiSonii@users.noreply.github.com>
2024-07-13 17:34:52 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 
-
-
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -8,6 +7,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <Button>
+  <Link href="/login">Login</Link>
+</Button>
+
     </div>
   );
 }
