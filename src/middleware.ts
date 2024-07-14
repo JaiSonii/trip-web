ea906c3d1b3dd7d2d922a134d2d5b89f6234c8e5
Jai Soni <138607981+JaiSonii@users.noreply.github.com>
2024-07-14 14:16:57 +0530

    Add files via upload

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
-import { verifyToken } from './utils/auth'
  
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const token = request.cookies.get('auth_token')?.value
 
   const loggedInUserNotAccessPaths = request.nextUrl.pathname === '/login' || request.nextUrl.pathname == '/'
@@ -32,7 +31,6 @@ export const config = {
     '/user/parties/:path/party-details',
     '/user/parties/:path/passbook',
     '/user/parties/:path/trips',
-    '/api/:path*'
   ]
 }
 // See "Matching Paths" below to learn more
